3235f3ae43f1e07fd7ced9c2d0bbb6c66f57b6bb
Patrick Kilgore <git@pck.email>
2022-06-24 19:12:06 -0500

    feat: server, tooling

---
+++ app.ml
@@ -0,0 +1,8 @@
+let () =
+  Dream.run ~interface:"0.0.0.0"
+  @@ Dream.logger
+  @@ Dream.router
+       [ Dream.get "/" (fun _ ->
+             Dream.html "Dream started by Docker Compose, built with opam!")
+       ]
+;;
